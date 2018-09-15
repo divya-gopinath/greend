87a2dd457eb790febeeb6f4b11b5e7dd08e5cd19
Divya Gopinath <divyagop@mit.edu>
2018-09-15 19:00:17 -0400

    Updated ticker queries

--- ticker_query.py
+++ ticker_query.py
@@ -2,12 +2,16 @@ from config import *
 import urllib.parse
 import requests 
 import json
+from yahoo_getter import cacheify
 
+@cacheify('tickers')
 def get_ticker_yahoo(equity):
 		response = requests.get( YAHOO_ENDPOINT.format(equity) ).text
 		response = response[42:-2]
 		response = json.loads(response)
-		return response['ResultSet']['Result'][0]['symbol']
+		if len(response['ResultSet']['Result']) != 0:
+			return response['ResultSet']['Result'][0]['symbol']
+		return None	
 
 
 def get_ticker_kensho(equity):
@@ -15,9 +19,5 @@ def get_ticker_kensho(equity):
 		results = response["data"]
 		# If there is a ticker name, return it.
 		if len(results) != 0 and "ticker_name" in results[0]:
-			return best_result["ticker_name"]
+			return results[0]["ticker_name"]
 		return None
-# format YAHOO_ENDPOINT with url-encoded string that we want to search for
-# gives back some json, get the first result.
-
-get_ticker_yahoo('Apple')
