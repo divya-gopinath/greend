35e7e7a83debd7a1a51f5a10de8614e8c0c9639c
Rohan Kodialam <rkodialam@gmail.com>
2018-09-15 16:49:55 -0400

    - Added pseudocode for the ticker query process

---
+++ ticker_query.py
@@ -0,0 +1,3 @@
+YAHOO_ENDPOINT = 'http://d.yimg.com/aq/autoc?query={}=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks'
+# format YAHOO_ENDPOINT with url-encoded string that we want to search for
+# gives back some json, get the first result.
