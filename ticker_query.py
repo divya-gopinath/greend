d7f64b83b2eb62139e6260c66f2f9fbc5a54ddc3
Justin Lim <justinlimkz@gmail.com>
2018-09-15 20:54:27 -0400

    ticker_query update

--- ticker_query.py
+++ ticker_query.py
@@ -8,17 +8,27 @@ def get_ticker_yahoo(equity):
     response = requests.get(YAHOO_ENDPOINT.format(equity)).text
     response = response[42:-2]
     response = json.loads(response)
-    return response['ResultSet']['Result'][0]['symbol']
-
+    try:
+        return response['ResultSet']['Result'][0]['symbol']
+    except:
+        return None
+        
 
 def get_ticker_kensho(equity):
     response = requests.get(KENSHO_GRAPH_API['ENDPOINT'] + equity,
                             headers={'Authorization': KENSHO_GRAPH_API['TOKEN']}).json()
-    results = response["data"]
-    if len(results) != 0 and "ticker_name" in results[0]:
-        return results[0]["ticker_name"]
-    return None
-
+    try:
+        results = response["data"]
+        if len(results) != 0:
+            for item in results:
+                if not any(char.isdigit() for char in item["ticker_name"]):
+                    return item["ticker_name"]
+        return None
+    except:
+        return None
+        
 
 if __name__ == '__main__':
-    get_ticker_yahoo('Apple')
+    print(get_ticker_yahoo('Apple'))
+    print(get_ticker_yahoo("Mcdonalds"))
+    print(get_ticker_kensho("Mcdonald's"))
